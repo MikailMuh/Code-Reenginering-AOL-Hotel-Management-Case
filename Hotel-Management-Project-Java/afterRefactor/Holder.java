711b0a206d19e254739558c8f8f6d5291e20a525
MikailMuh <mikail.2018.7@gmail.com>
2026-05-19 14:54:21 +0700

    first commit: belum ada yang direfactor baru pemisahan class

---
+++ Hotel-Management-Project-Java/afterRefactor/Holder.java
@@ -0,0 +1,8 @@
+package afterRefactor;
+
+public class Holder {
+    Doubleroom luxury_doublerrom[]=new Doubleroom[10]; 
+    Doubleroom deluxe_doublerrom[]=new Doubleroom[20]; 
+    Singleroom luxury_singleerrom[]=new Singleroom[10]; 
+    Singleroom deluxe_singleerrom[]=new Singleroom[20]; 
+}
